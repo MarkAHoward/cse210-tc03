84571c7dddec64ac6c1a6575b23bd5615a5fe3b6
allithegreat88 <allisonbday@byui.edu>
2021-01-22 15:09:47 -0700

    This one works

--- colored_bios.py
+++ colored_bios.py
@@ -1,4 +1,6 @@
 from colorama import Fore, Style
 
 
-allison-bio = Fore.GREEN + "Hello! My name is Allison Day. I am 18 years old. I am terrible at Monopoly but passable at Ticket-to-Ride. I have a dog named Barry who ate my earbud once. It still hasn't come out." + Style.RESET_ALL
+allison_bio = Fore.GREEN + "Hello! My name is Allison Day. I am 18 years old. I am terrible at Monopoly but passable at Ticket-to-Ride. I have a dog named Barry who ate my earbud once. It still hasn't come out." + Style.RESET_ALL
+
+print(allison_bio)
