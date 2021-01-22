3c2034be49189ccc6c7e16e8b74b17e604a37d53
allithegreat88 <allisonbday@byui.edu>
2021-01-22 15:04:50 -0700

    pretty colors

--- colored_bios.py
+++ colored_bios.py
@@ -1,1 +1,4 @@
 from colorama import Fore, Style
+
+
+allison-bio = Fore.GREEN + "Hello! My name is Allison Day. I am 18 years old. I am terrible at Monopoly but passable at Ticket-to-Ride. I have a dog named Barry who ate my earbud once. It still hasn't come out." + Style.RESET_ALL
