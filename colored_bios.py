8365d9e96b953b1ebd05720b0a56988e6ab4b81c
Conner Haar <77696186+Connerhaar@users.noreply.github.com>
2021-01-22 15:07:22 -0700

    colored text

--- colored_bios.py
+++ colored_bios.py
@@ -1,1 +1,3 @@
 from colorama import Fore, Style
+conner_color = Fore.BLUE +  "I am from the Lincoln California. This is my third semester at BYU-I but my second semester on campus. I like playing and watching basketball.Im the youngest of 4 siblings, 2 sister and one brother." + Style.RESET_ALL
+print (conner_color)
